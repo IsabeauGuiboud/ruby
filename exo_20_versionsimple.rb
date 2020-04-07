739c97072919906ab88d6ed9112faf53e0123c56
Isabeau <isabeau.guiboud@gmail.com>
2020-04-07 17:16:36 +0200

    ajout condition etages

--- exo_20_versionsimple.rb
+++ exo_20_versionsimple.rb
@@ -1,8 +1,15 @@
 pyramide = ["#"]
 composition = ["1"]
+
 puts "Salut, bienvenue dans ma pyramide! Combien d'étages veux-tu? Saisis un nombre entre 1 et 25"
 print ">"
 etages=gets.chomp.to_i
+
+if etages < 1 || etages > 25
+  puts "Mets un étage supérieur à 1 ou inférieur à 25 stp"
+  print ">"
+  etages=gets.chomp.to_i
+end
 
 for i in 1..etages
   composition << i
